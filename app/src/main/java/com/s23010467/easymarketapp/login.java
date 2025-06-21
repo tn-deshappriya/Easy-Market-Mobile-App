409a35e038ae26adef85072e49c6f3eca6ef676a
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2025-06-21 18:22:51 +0530

    Created Loading Page,Signup,SignIn and get UI for Dashboard.

---
+++ app/src/main/java/com/s23010467/easymarketapp/login.java
@@ -0,0 +1,40 @@
+package com.s23010467.easymarketapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class login extends AppCompatActivity {
+    // Define variable for get login page registration button value assign..
+    private Button loginRegisterBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Button Value extract to variable
+        loginRegisterBtn = findViewById(R.id.loginRegisterBtn);
+
+        // Create nevigate to Register page through login page Register button
+        loginRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login.this, register.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
